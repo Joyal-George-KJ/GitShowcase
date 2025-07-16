283edf6f759ec6967e2d15360a4c35dda3501618
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-07-16 19:45:19 +0530

    Home page Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
 import HomePage from "./HomePage";
 
 export default function Home() {
-    return (
-        <>
-            <HomePage />
-        </>
-    );
+    return <HomePage />;
 }

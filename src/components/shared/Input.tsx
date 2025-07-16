8d168aa2eb138906d76622d9ba231b76d41a670e
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-07-16 19:47:19 +0530

    Input component done

---
+++ src/components/shared/Input.tsx
@@ -0,0 +1,10 @@
+export default function Input({type, placeholder, required = false} : Readonly<{type: string; placeholder: string; required: boolean}>) {
+    return (
+        <input
+            required={required}
+            type={type}
+            placeholder={placeholder}
+            className="w-full p-3 border-b focus:border-b-4 border-neutral-500 text-white outline-none pl-0"
+        />
+    );
+}
